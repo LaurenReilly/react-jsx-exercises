fc574869517118d96fbfcefe704eeccdc1a28151
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-11 11:04:21 -0500

    finished form exercise

--- form/app.js
+++ form/app.js
@@ -1,17 +1,52 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    const style = {
+        border: "10px solid green",
+        margin: "2em",
+        width: "200px",
+        height: "100px"
+    }
+    return <input type="text" style={style}/>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    const style = {
+        color: 'green',
+        marginRight: "5em"
+    }
+    return  <div style={{margin: "0 auto", width: "30%"}}>
+                <input type="radio" id="yes" name="answer" value="yes"/>
+                <label htmlFor="yes" style={style}>YES</label>
+                <input type="radio" id="no" name="answer" value="no"/>
+                <label htmlFor="no" style={style}>NO</label>
+            </div>
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    const style = {
+        borderRadius: "5px",
+        border: "10px solid green",
+        backgroundColor: "orchid",
+        padding: "1em",
+        fontWeight: "bold",
+        color: "white"
+    }
+    return  <button type="submit" style={style}>SUBMIT</button>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    return  <div style={{margin: "0 auto", width: "90%", display: "flex", justifyContent:"center"}}>
+                <form>
+                    <div style={{margin:"0 auto", display: "flex", justifyContent: "space-around"}}>
+                        <TextInput/>
+                        <TextInput/>
+                        <TextInput/>
+                    </div>
+                    <YesNoRadio/>
+                    <div style={{margin:"0 auto", display: "flex", justifyContent: "center"}}>
+                        <SubmitButton/>
+                    </div>
+                </form>
+            </div>
 }
 
 
